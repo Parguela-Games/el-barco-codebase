23c1fa8fdbdd9b31b31f953cd3aee2f4c97217b9
Ale Sanchez <alesanchezmedina@gmail.com>
2022-01-13 20:03:15 +0100

    Tried to fix the animations

--- ActionableObjects/StandardActionableObject.cs
+++ ActionableObjects/StandardActionableObject.cs
@@ -3,40 +3,37 @@ using System.Collections.Generic;
 using Manicomio.Animation;
 using UnityEngine;
 
-namespace Manicomio.ActionableObjects
-{
-  [RequireComponent(typeof(Animator))]
-  public class StandardActionableObject : ActionableObject
-  {
-    [Header("Animation")]
-    [Tooltip("The animation state to play when interacting with this object")]
-    [SerializeField] string animationState;
+namespace Manicomio.ActionableObjects {
+    [RequireComponent(typeof(Animator))]
+    public class StandardActionableObject : ActionableObject {
+        [Header("Animation")]
+        [Tooltip("The animation state to play when interacting with this object")]
+        [SerializeField] string animationState;
 
-    protected Animator m_animator;
+        protected Animator m_animator;
 
-    protected override void InnerStart()
-    {
-      m_animator = GetComponent<Animator>();
+        protected override void InnerStart() {
+            m_animator = GetComponent<Animator>();
+        }
+
+        protected override void InnerInteract() {
+            UpdateAnimation();
+        }
+
+        private void UpdateAnimation() {
+            if (m_interacted) {
+                m_animator.ResetTrigger("interact");
+            } else {
+                m_animator.SetTrigger("interact");
+            }
+        }
+
+        public override bool IsAnimationPlaying() {
+            var state = m_animator.GetCurrentAnimatorStateInfo(0);
+
+            Debug.Log(state.fullPathHash);
+
+            return state.IsName(AnimationConstants.ANIMATION_GENERIC_END);
+        }
     }
-
-    protected override void InnerInteract()
-    {
-      UpdateAnimation();
-    }
-
-    private void UpdateAnimation()
-    {
-      m_animator.SetFloat(AnimationConstants.ANIMATION_DIRECTION, !m_interacted ? 1f : -1f);
-      m_animator.Play(animationState);
-    }
-
-    public override bool IsAnimationPlaying()
-    {
-      var state = m_animator.GetCurrentAnimatorStateInfo(0);
-
-      Debug.Log(state.fullPathHash);
-
-      return state.IsName(AnimationConstants.ANIMATION_GENERIC_END);
-    }
-  }
 }
